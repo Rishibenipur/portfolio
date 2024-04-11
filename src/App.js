59218f606ea8d9615010e47268975109633a6fcd
babulvaibhav <babulvaibhav2001@gmail.com>
2024-04-11 22:57:59 +0530

    Started Routing of Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-function App() {
+import React,{Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/HomePages";
 
-  return (
-    <div className="text-red-900 font-bold">
-       Hello Rea
-    </div>
-  );
 
+//import Component from "./components/Components";
+
+// function App() {
+
+//   return (
+//     <div className="text-red-900 font-bold">
+//        Hello Rea
+//     </div>
+//   );
+
+// }
+class App extends Component {
+  render() {
+      return (
+          <Router>
+              <div className="App ">
+                  <ul className="App-header">
+                      <li>
+                          <Link to="/HomePages">Home</Link>
+                      </li>
+                      
+                  </ul>
+                  <Routes>
+                      <Route path="/HomePages" element={<Home/>}></Route>
+                  </Routes>
+              </div>
+          </Router>
+      );
+  }
 }
 
 export default App;
--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Components from "../components/Components"
 const HomePages = () => {
   return (
