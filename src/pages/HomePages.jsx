87faf474a276929aeedf88f064dd7495e5947641
babulvaibhav <babulvaibhav2001@gmail.com>
2024-04-11 23:40:08 +0530

    slight changes in home

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -3,7 +3,9 @@ import Components from '../components/Components';
 const HomePages = () => {
   return (
     <>
-      <div className="bg-slate-100">This is temporary HomePages where i have used my component</div>
+      <div className="bg-slate-100">
+        This is temporary HomePages where i have used my component
+      </div>
       <Components />
     </>
   );
